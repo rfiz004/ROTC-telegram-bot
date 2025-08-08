0ed0899f3dfebc157b3d6dd8e5837026f09a9018
Arefe Izadi Tabar <izaditabar.arefeh@gmail.com>
2025-08-08 10:55:59 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # ────────────── Token Configs
-BOT_TOKEN = os.environ.get("BOT_TOKEN","8135083594:AAHb7wdYeUsVnNaCjPoP9ZYSTlDSJGcJhEs")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 if not BOT_TOKEN:
     raise ValueError("❌ BOT_TOKEN is missing!")
 
